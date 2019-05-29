bf1533ec2dad8107cbc74589cfe26a745d4a3764
HarryWei530 <13270917+HarryWei530@users.noreply.github.com>
2019-05-29 13:51:34 -0400

    Update and rename Kmeansbin.m to Kmeanspic.m

--- Kmeanspic.m
+++ Kmeanspic.m
@@ -1,4 +1,5 @@
-function cluster_center = Kmeansbin(filename,number,cluster_center,first_iter,rect,pausetime)
+function cluster_center = Kmeanspic(filename,number,cluster_center,first_iter,rect,pausetime)
+% Performs Kmeans on LAB colour space, save clusters in grayscale
 rng(1)
 warning('off', 'images:initSize:adjustingMag')
 im = imread(sprintf('%s (%d).jpg',filename,number));
@@ -21,7 +22,7 @@ ab = double(lab_he(:,:,2:3));
 nrows = size(ab,1);
 ncols = size(ab,2);
 ab = reshape(ab,nrows*ncols,2);
-nColors = 8; % Setting number of clusters
+nColors = 5; % Setting number of clusters
 if first_iter
 [cluster_idx, cluster_center] = kmeans(ab,nColors,'distance','sqeuclidean', ...
                                       'Replicates',1);

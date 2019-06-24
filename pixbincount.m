f0ed17482089017b097ff10eac2473a012612814
HarryWei530 <13270917+HarryWei530@users.noreply.github.com>
2019-06-24 11:43:46 -0400

    Update pixbincount.m

--- pixbincount.m
+++ pixbincount.m
@@ -4,20 +4,22 @@ function pix = pixbincount(filename,number);
 I = imread(sprintf('%s(%d).bmp',filename,number));
 
 [row col] = size(I);
-row = uint8(row/2);
-col = uint8(col/2);
+row = uint16(row/2);
+col = uint16(col/2);
 
 %% Binarize image by setting inner most ring as one, rest as zeros
 center = I(row,col); 
 I(I ~= center) = 0;
 I(I == center) = 1;
 I = logical(I);
+
 %% Gnerate convex hull, select only center hull, save image and generate pixel count of hull
-I = bwconvhull(I,'objects'); 
+I = bwconvhull(I,'objects');
 I = bwlabel(I);
-center = I(row,col); 
+center = I(row,col);
 I(I ~= center) = 0;
 I(I == center) = 1;
+
 pix = sum(sum(I));
 
 imshow(I),title(sprintf('BW %s (%d).bmp',filename,number));

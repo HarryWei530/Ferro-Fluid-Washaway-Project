fcaaaa778839aeb3ced142dea83ea07508d1488f
HarryWei530 <13270917+HarryWei530@users.noreply.github.com>
2020-02-05 22:22:35 -0500

    Optimize binarization
    Now instead of look at the center only pixel, binarization looks at a 100*100 region, picks out color that occurred the most as binarizing color

--- pixbincount.m
+++ pixbincount.m
@@ -8,7 +8,21 @@ row = uint16(row/2);
 col = uint16(col/2);
 
 %% Binarize image by setting inner most ring as one, rest as zeros
-center = I(row,col); 
+center_rect = I(row-100:row+100,col-100:col+100);
+elements = (unique(center_rect))';
+if (length(elements)~=1)
+    elements = uint16(cat(1,elements,zeros(size(elements))));
+    for i = 1:length(elements)
+        temp = center_rect;
+        temp(temp ~= elements(1,i)) = 0;
+        temp(temp == elements(1,i)) = 1;
+        elements(2,i) = sum(sum(temp));
+    end
+    center = max(elements(1,:));
+else
+    center = elements;
+end
+
 I(I ~= center) = 0;
 I(I == center) = 1;
 I = logical(I);
